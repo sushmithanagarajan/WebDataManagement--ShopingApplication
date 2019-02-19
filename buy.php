771c616788fb7996a7bda7676f5b321d526b4f35
Sushmitha Nagarajan <38819267+sushmithanagarajan@users.noreply.github.com>
2019-02-19 02:17:27 -0600

    Update buy.php

--- buy.php
+++ buy.php
@@ -6,7 +6,7 @@
 	<?php 
 		error_reporting(E_ALL);
 		ini_set('display_errors', 'On');
-		$xmlstr = file_get_contents('http://sandbox.api.ebaycommercenetwork.com/publisher/3.0/rest/CategoryTree?apiKey=78b0db8a-0ee1-4939-a2f9-d3cd95ec0fcc&visitorUserAgent&visitorIPAddress&trackingId=7000610&categoryId=72&showAllDescendants=true');
+		$xmlstr = file_get_contents('http://sandbox.api.ebaycommercenetwork.com/publisher/3.0/rest/CategoryTree?apiKey=&visitorUserAgent&visitorIPAddress&trackingId=7000610&categoryId=72&showAllDescendants=true');
 		$ebayXmlContent = new SimpleXMLElement($xmlstr);
 		session_start();
 		
